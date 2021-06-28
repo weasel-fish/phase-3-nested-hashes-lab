8d6561a0b287658d6e79ae8d7dccbc65d6740605
github username <github email address>
2021-06-28 15:17:29 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+    programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +88,10 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = { known_for: "Ruby", languages: ["Lisp", "C"]}
+
+  programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +114,10 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+
+    programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +139,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+    programmer_hash
 
 end
